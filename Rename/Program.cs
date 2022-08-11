3b58c7d91ffc464fc3b05d64cbfe5421a4a65109
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-11 11:47:52 +0530

    UC2 is added

--- Rename/Program.cs
+++ Rename/Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("Hello, World!");
 
             //UC1 call
+
+            //UC2 Call
         }
     }
 }
---
+++ Rename/UC2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rename
+{
+    internal class UC2
+    {
+        public void UcSample()
+        {
+            Console.Write("this is a sample UC2");
+        }
+    }
+}
